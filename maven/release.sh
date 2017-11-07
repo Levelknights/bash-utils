dc2796014f609683b10e3ad414a4e500c8ba7f66
Dariusz Raćkowski <dariusz.rackowski@rb.com>
2017-11-07 16:32:37 +0100

    merging old scripts versions - fist step

---
+++ maven/release.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+function mvnExpression {
+    mvn help:evaluate -Dexpression=$1 | grep -v -e "\[INFO"
+}
+
+function lastBuildNumberForRcPrefix {
+    MINOR_VER=$1
+    RESULT=-1
+    for i in $(git branch | grep "${MINOR_VER}"); do
+        i="${i##*.}"
+        RESULT=$(( $i > $RESULT ? $i : $RESULT ))
+    done
+    echo ${RESULT}
+}
+
+BRANCH_PREFIX="rc/"
+PROFILES="inpar,deploy"
+
+export ARTIFACT_ID="$(mvnExpression "project.artifactId")"
+export CURRENT_VER="$(mvnExpression "project.version")"
+export MINOR_VER="$(echo ${CURRENT_VER} | sed "s/-SNAPSHOT//g")"
+export RC_BRANCH_PREFIX="rc/${MINOR_VER}"
+export NEXT_BUILD_NUMBER=$(( $(lastBuildNumberForRcPrefix ${RC_BRANCH_PREFIX}) + 1))
+export NEXT_VER="$MINOR_VER.$NEXT_BUILD_NUMBER"
+export RC_BRANCH="$RC_BRANCH_PREFIX.$NEXT_BUILD_NUMBER"
+
+echo "PROFILES    = $PROFILES"
+echo "ARTIFACT_ID = $ARTIFACT_ID"
+echo "CURRENT_VER = $CURRENT_VER"
+echo "MINOR_VER = $MINOR_VER"
+echo "RC_BRANCH_PREFIX = $RC_BRANCH_PREFIX"
+echo "NEXT_BUILD_NUMBER = $NEXT_BUILD_NUMBER"
+echo "NEXT_VER = $NEXT_VER"
+echo "RC_BRANCH = $RC_BRANCH"
+
+echo "[INFO] -----------------------------------------------------------"
+
+BUILD_USER=$1
+if [ "$BUILD_USER" == "" ]; then
+    BUILD_USER="-";
+fi
+
+echo "BUILD_USER=${BUILD_USER}"
+
+CURRENT_BRANCH=$(git symbolic-ref HEAD 2>/dev/null)
+if [ "$CURRENT_BRANCH" != "refs/heads/master" ]; then
+    git branch
+    echo "[ERROR] You are not at \"refs/heads/master\" branch!";
+    exit 1
+fi
+
+CHANGED=$(git diff-index --name-only HEAD --)
+if [ -n "$CHANGED" ]; then
+    git status
+    echo "[ERROR] Changes found!";
+    exit 1
+fi
+
+if [ "$JENKINS_URL" == "" ]; then
+    read -p "Are you sure? " -n 1 -r
+    if [[ $REPLY =~ ^[^Yy]$ ]]; then
+      echo
+      exit 1;
+    fi
+    echo
+fi
+
+echo "[INFO] setup new version in pom"
+git branch "${RC_BRANCH}" \
+    && git checkout "${RC_BRANCH}" \
+    && mvn -B versions:set -DnewVersion="${NEXT_VER}" -DgenerateBackupPoms=false \
+    && git add -A \
+    && git commit -m "release ${NEXT_VER} (by ${BUILD_USER})"
+
+echo "[INFO] checkout tag \"${RC_BRANCH}\" and perform DEPLOY to repository with profiles \"${PROFILES}\""
+git checkout "${RC_BRANCH}" && mvn deploy -P "${PROFILES}" -DskipTests=true
+
+echo "[INFO] push changes to SCM"
+git push origin --follow-tags
+
+echo "[SUCCESS] release SUCCESS"
+git checkout master --force && git clean -f -d && mvn release:clean
+
---
+++ maven/variables.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+function mvnExpression {
+    mvn help:evaluate -Dexpression=$1 | grep -v -e "\[INFO"
+}
+
+function end_with() {
+	echo "$*" >&2
+	exit 1
+}
+
+function usage() {
+	echo "Maven git variables preparation v1.0 (c) 2017 Levelknights"
+	echo ""
+	echo "  -p    Sets profiles variable (default 'deploy')"
+	echo "  -o    Prints variables to output"
+	echo "  -s    Release version strategy: cut_snapshot, last_tag, manual"
+	echo "  -t    Release tag (default 'v$RELEASE_VER')"
+	echo "  -v    If strategy manual, then release version"
+	echo "  -n    If strategy manual, then next version (if -SNAPSHOT wanted needs to be passed)"
+	echo ""
+	echo "  -h    For this message"
+	echo ""
+}
+
+OUTPUT=false
+export PROFILES="deploy"
+export ARTIFACT_ID="$(mvnExpression "project.artifactId")"
+export CURRENT_VER="$(mvnExpression "project.version")"
+
+while getopts "hp:os:t:v:n:" opt; do
+	case "${opt}" in
+		h)
+			usage
+			exit 0
+			;;
+		p) #Profiles
+			PROFILES="${OPTARG}"
+			;;
+		o) #Output
+			OUTPUT=true
+			;;
+		s) #Strategy
+			_STRATEGY="${OPTARG}"
+			;;
+		t) #Tag name
+			_TAG_NAME="${OPTARG}"
+			;;
+		v) #Release version
+			_RELEASE_VERSION="${OPTARG}"
+			;;
+		n) #Next
+			_NEXT_VERSION="${OPTARG}"
+			;;
+
+		*)
+			usage
+			end_with "Unknown parameter ${opt}"
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+#TODO strategies
+
+export RELEASE_VER="$(echo $CURRENT_VER | sed "s/-SNAPSHOT//g")"
+
+export NEXT_VER=${RELEASE_VER%.*}"."$(( ${RELEASE_VER##*.} + 1 ))"-SNAPSHOT"
+
+if [ "$_TAG_NAME" != "" ]; then
+	export TAG_NAME="${_TAG_NAME}"
+else 
+	export TAG_NAME="v$RELEASE_VER"
+fi
+
+
+if [ $OUTPUT ]; then
+	echo "PROFILES    = $PROFILES"
+	echo "ARTIFACT_ID = $ARTIFACT_ID"
+	echo "CURRENT_VER = $CURRENT_VER"
+	echo "RELEASE_VER = $RELEASE_VER"
+	echo "NEXT_VER    = $NEXT_VER"
+	echo "TAG_NAME    = $TAG_NAME"
+fi
